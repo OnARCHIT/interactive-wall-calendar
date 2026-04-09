9d344e895d441d36c763e84a8c10c561d101a17f
OnARCHIT <149319674+OnARCHIT@users.noreply.github.com>
2026-04-09 11:22:58 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import WallCalendar from "@/components/calendar/WallCalendar";
+
+
+export default function Page() {
+
+  return (
+
+    <div className="pageWrapper">
+
+      <WallCalendar />
+
+    </div>
+
+  );
+
+}
